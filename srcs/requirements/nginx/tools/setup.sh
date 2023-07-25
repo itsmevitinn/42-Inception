48704f974431130968ed1bcbde156b4bd4f0956d
Vitor Sergio <vitor@Vitors-MacBook-Air.local>
2023-07-25 17:04:57 -0300

    fix: change bash to sh in script + correct .env values

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e $SSL_CERTIFICATE >> /etc/nginx/nginx-selfsigned.crt
-echo -e $SSL_PRIVATE_KEY >> /etc/nginx/nginx-selfsigned.key
+echo "-----BEGIN CERTIFICATE-----" > /etc/nginx/nginx-selfsigned.crt
+echo $SSL_CERTIFICATE >> /etc/nginx/nginx-selfsigned.crt
+echo -n "-----END CERTIFICATE-----" >> /etc/nginx/nginx-selfsigned.crt
+
+echo "-----BEGIN PRIVATE KEY-----" > /etc/nginx/nginx-selfsigned.key
+echo $SSL_PRIVATE_KEY >> /etc/nginx/nginx-selfsigned.key
+echo -n "-----END PRIVATE KEY-----" >> /etc/nginx/nginx-selfsigned.key
 
 exec nginx
