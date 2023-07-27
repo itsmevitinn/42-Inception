b5d91511fd3cf5fa9b33bf6bd163e484571ce0ea
Vitor Sergio <vitor@Vitors-MacBook-Air.local>
2023-07-27 20:14:02 -0300

    feat: all containers connected, need test

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,1 +1,14 @@
 #!/bin/sh
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# Avoid socket errors
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+# Avoid groups owner errors
+chown -R mysql:mysql /var/lib/mysql
+
+echo "CREATE DATABASE $DB_NAME;" > init.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
+
+mysqld --user=mysql --init-file=/init.sql
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "/var/www/html" ]
 then
-	mkdir /var/www/html
+	mkdir -p /var/www/html
 fi
 
 cd /var/www/html
@@ -10,7 +10,7 @@ wget -O /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pag
 chmod +x /usr/local/bin/wp
 wp core download --allow-root --locale=en_US
 wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST}
-# wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
-# wp user create --allow-root ${WP_USER} ${WP_EMAIL} --user_pass=${WP_PASSWORD} --role=author
+wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
+wp user create ${WP_USER} ${WP_EMAIL} --user_pass=${WP_PASSWORD} --role=author
 
 exec php-fpm7 -F -R
