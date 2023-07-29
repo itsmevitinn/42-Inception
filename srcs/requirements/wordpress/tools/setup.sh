9db7b8853afaa190eae3c04dd8b4455bec1d0f32
Vitor Sergio <vitor@Vitors-MacBook-Air.local>
2023-07-28 21:12:23 -0300

    feat: allow any ip address connect in php fpm conf

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-if [ ! -d "/var/www/html" ]
-then
-	mkdir -p /var/www/html
-fi
-
+# Allow connections from any IP address
+sed -i 's#listen = 127.0.0.1:9000#listen = 0.0.0.0:9000#' /etc/php7/php-fpm.d/www.conf
+mkdir -p /var/www/html
 cd /var/www/html
-wget -O /usr/local/bin/wp https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x /usr/local/bin/wp
 wp core download --allow-root --locale=en_US
 wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST}
 wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email
